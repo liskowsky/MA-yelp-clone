701bd48e377d916838c19620d41c8ba3f720fdfc
liskowsky <karollebiedzinski@gmail.com>
2016-08-24 17:56:33 +0100

    Challenge finished

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,12 +1,6 @@
 class Restaurant < ActiveRecord::Base
   belongs_to :user
-  has_many :reviews, dependent: :destroy
+  has_many :reviews, -> { extending WithUserAssociationExtension }, dependent: :destroy
   validates :name, length: { minimum: 3 }, uniqueness: true
 
-  def build_review(review_params, user)
-    review = Review.new(review_params)
-    review.user = user
-    review.restaurant = self
-    review
-  end
 end
